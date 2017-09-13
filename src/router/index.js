8e1029674fdca31ee2fae6435d23e384db176c38
moziyuan <mqk-kovel@foxmail.com>
2017-09-13 10:11:36 +0800

    同步

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Tab from '../components/Hello.vue'
 Vue.use(Router)
 
 export default new Router({

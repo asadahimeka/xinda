b7d9267c5900fad53b620fffab17fdac2e6501b5
173271328 <liugaofeng5178@163.com>
2017-09-28 10:45:57 +0800

    按键修改

--- src/components/v-page.vue
+++ src/components/v-page.vue
@@ -15,65 +15,66 @@
 
 <script>
 export default {
-    props: {
-      curInx: {
-        type: [String, Number],
-        required: true
-      },
-      pageSize: {
-        type: [String, Number],
-        required: true
-      },
-      totalShow: {
-        type: [Boolean],
-        required: true
-      },
-      pageChange: {
-        default() {
-          return function pageChange() {
-            // TODO
-          }
-        }
-      }
+  props: {
+    curInx: {
+      type: [String, Number],
+      required: true
     },
-    computed: {
-      indexs() {
-        var left = 1
-        var right = this.pageSize
-        var ar = [] 
-        if (this.pageSize >= 11) {
-          if (this.curInx > 5 && this.curInx < this.pageSize - 4) {
-            left = this.curInx - 5
-            right = this.curInx + 4
-          } else {
-            if (this.curInx <= 5) {
-              left = 1
-              right = 10
-            } else {
-              right = this.pageSize
-              left = this.pageSize -9
-            }
-          }
-        }
-        while (left <= right) {
-          ar.push(left)
-          left ++
-        }   
-        return ar
-      }
+    pageSize: {
+      type: [String, Number],
+      required: true
     },
-    methods: {
-      btnClick(page) {
-        if (page != this.curInx) {
-          this.pageChange(page)
+    totalShow: {
+      type: [Boolean],
+      required: true
+    },
+    pageChange: {
+      default() {
+        return function pageChange() {
+          // TODO
         }
       }
     }
+  },
+  computed: {
+    indexs() {
+      var left = 1
+      var right = this.pageSize
+      var ar = []
+      if (this.pageSize >= 11) {
+        if (this.curInx > 5 && this.curInx < this.pageSize - 4) {
+          left = this.curInx - 5
+          right = this.curInx + 4
+        } else {
+          if (this.curInx <= 5) {
+            left = 1
+            right = 10
+          } else {
+            right = this.pageSize
+            left = this.pageSize - 9
+          }
+        }
+      }
+      while (left <= right) {
+        ar.push(left)
+        left++
+      }
+      return ar
+    }
+  },
+  methods: {
+    btnClick(page) {
+      if (page != this.curInx) {
+        this.pageChange(page)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less">
-ul,li {
+ul,
+li {
   margin: 0px;
   padding: 0px;
 }
@@ -102,7 +103,14 @@ ul,li {
 }
 
 .page-bar button:hover {
-    background-color: #eee;
+  background-color: #eee;
+}
+
+.page-bar button[disabled] {
+  cursor: not-allowed;
+  &:hover {
+    background-color: #fff;
+  }
 }
 
 .page-bar .active button {
@@ -113,17 +121,20 @@ ul,li {
 }
 
 .page-bar i {
-  font-style:normal;
+  font-style: normal;
   color: #2595d0;
   margin: 0px 4px;
   font-size: 12px;
 }
 
-.total{
-  cursor: default !important; 
+.total {
+  cursor: default !important;
   &:hover {
     background-color: #fff !important;
   }
 }
 
+button {
+  outline: none;
+}
 </style>
